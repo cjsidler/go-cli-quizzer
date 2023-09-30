1ad6548aac95b261bd250393fb7e7f4db5ee18e3
cjsidler <38596474+cjsidler@users.noreply.github.com>
2023-09-30 15:27:07 -0700

    cleanup sample problems and add plan

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+func main() {
+	// read in quiz via CSV file
+	// user can specify '-csv' flag and file name
+	// otherwise, default to 'problems.csv'
+
+	// ask user all questions, keep track of score
+
+	// output number of correct responses vs total questions
+}
