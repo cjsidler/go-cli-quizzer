49d1bd91cf60a93e34070a598adbfd8adb781612
cjsidler <38596474+cjsidler@users.noreply.github.com>
2023-09-30 15:54:58 -0700

    open and print each line of csv

--- main.go
+++ main.go
@@ -1,11 +1,45 @@
 package main
 
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+/*
+read in quiz via CSV file
+user can specify '-csv' flag and file name
+otherwise, default to 'problems.csv'
+
+ask user all questions, keep track of score
+
+output number of correct responses vs total questions
+*/
+
 func main() {
-	// read in quiz via CSV file
-	// user can specify '-csv' flag and file name
-	// otherwise, default to 'problems.csv'
+	// Open problems.csv
+	csvFile, err := os.Open("problems.csv")
 
-	// ask user all questions, keep track of score
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	// output number of correct responses vs total questions
+	csvReader := csv.NewReader(csvFile)
+
+	// Read and print every line of the csv
+	for {
+		record, err := csvReader.Read()
+
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println(record)
+	}
 }
